8be950edb8faa450aa05ff09045193b6ca6f6d73
yohpan <yohanpaya74@hotmail.fr>
2024-11-19 20:15:34 +0100

    add total of the account on account_screen Fixes #45

--- back/back_account_managment/permissions.py
+++ back/back_account_managment/permissions.py
@@ -2,20 +2,8 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-    def has_permission(self, request, view):
-        model_id = view.kwargs.get("pk", None)
-
-        if request.method not in ["GET", "PATCH", "PUT", "DELETE"]:
+    def has_object_permission(self, request, view, obj):
+        if getattr(obj, "user", None) is None:
             return True
 
-        if request.method == "GET" and model_id is None:
-            return True
-
-        try:
-            model = view.queryset.model
-
-            ressource = model.objects.get(pk=model_id)
-        except model.DoesNotExist:
-            return False
-
-        return ressource.user == request.user
+        return obj.user == request.user
