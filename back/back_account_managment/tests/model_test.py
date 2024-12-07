1d7c325d8b5ea10dc0d1d0c01f26ca7715943066
YohPaN <yohanpaya74@hotmail.fr>
2024-12-07 11:43:11 +0100

    implement coverage + models tests

--- back/back_account_managment/tests/model_test.py
+++ back/back_account_managment/tests/model_test.py
@@ -1,31 +1,65 @@
+from back_account_managment.models import Account, AccountUser, Item
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 from django.test import TestCase
+
+User = get_user_model()
 
 
 class UserModelTest(TestCase):
-    def setUp(cls):
-        pass
+    """Nothing to test"""
 
 
 class ProfileModelTest(TestCase):
-    def setUp(cls):
-        pass
+    """Nothing to test"""
 
 
 class AccountModelTest(TestCase):
     def setUp(cls):
-        pass
+        cls.user = User.objects.create(username="JonDoe", email="jon@doe.test")
+        cls.account = Account.objects.create(id=1, name="test", user=cls.user)
+
+    def test_total_property(self):
+        for i in range(3):
+            Item.objects.create(
+                title="test",
+                description="test",
+                valuation=12,
+                account=self.account,
+            )
+
+        account = Account.objects.get(pk=1)
+        self.assertEqual(account.total["total_sum"], 36)
 
 
 class ItemModelTest(TestCase):
-    def setUp(cls):
-        pass
+    """Nothing to test"""
 
 
 class AccountUserModelTest(TestCase):
     def setUp(cls):
-        pass
+        cls.user = User.objects.create(username="JonDoe", email="jon@doe.test")
+        cls.account = Account.objects.create(id=1, name="test", user=cls.user)
+
+    def test_create_account_user_with_bad_state(self):
+        with self.assertRaises(IntegrityError):
+            AccountUser.objects.create(
+                state="BAD_STATE", user=self.user, account=self.account
+            )
+
+    def test_create_account_user_with_all_state_possible(self):
+        AccountUser.objects.create(
+            state="PENDING", user=self.user, account=self.account
+        )
+        AccountUser.objects.create(
+            state="APPROVED", user=self.user, account=self.account
+        )
+        AccountUser.objects.create(
+            state="DISAPPROVED", user=self.user, account=self.account
+        )
+
+        self.assertEqual(len(AccountUser.objects.all()), 3)
 
 
 class AccountUserPermissionModelTest(TestCase):
-    def setUp(cls):
-        pass
+    """Nothing to test"""
