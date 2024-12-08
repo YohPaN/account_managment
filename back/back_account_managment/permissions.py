da26640f566d3e79e3c9eb2a60314e2a7d8de8e0
YohPaN <yohanpaya74@hotmail.fr>
2024-12-08 13:09:05 +0100

    add isOwner test

--- back/back_account_managment/permissions.py
+++ back/back_account_managment/permissions.py
@@ -8,8 +8,8 @@ User = get_user_model()
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if getattr(obj, "user", None) is None:
-            return True
+        # if getattr(obj, "user", None) is None:
+        #     return True
 
         return obj.user == request.user
 
--- back/back_account_managment/tests/permission_test.py
+++ back/back_account_managment/tests/permission_test.py
@@ -1,9 +1,46 @@
+from unittest.mock import Mock
+
+from back_account_managment.permissions import IsOwner
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+User = get_user_model()
 
 
 class IsOwnerPermissionTest(TestCase):
-    def setUp(cls):
-        pass
+    def setUp(self):
+        self.user = User.objects.create(
+            username="JonDoe", email="jon@doe.test"
+        )
+
+        self.user2 = User.objects.create(
+            username="FooBar", email="foo@bar.test"
+        )
+
+        factory = APIRequestFactory()
+        self.request = factory.get("/")
+        self.request.user = self.user
+
+    def test_object_have_permission(self):
+        obj = Mock()
+        obj.user = User.objects.get(username="JonDoe")
+
+        isOwner = IsOwner.has_object_permission(
+            self=None, request=self.request, view=None, obj=obj
+        )
+
+        self.assertTrue(isOwner)
+
+    def test_object_have_not_permission(self):
+        obj = Mock()
+        obj.user = User.objects.get(username="FooBar")
+
+        isOwner = IsOwner.has_object_permission(
+            self=None, request=self.request, view=None, obj=obj
+        )
+
+        self.assertFalse(isOwner)
 
 
 class IsContributorPermissionTest(TestCase):
