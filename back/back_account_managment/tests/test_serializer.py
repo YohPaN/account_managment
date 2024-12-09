4739ed5b59f21bc9fdba7b1d41347ed37b0f4092
YohPaN <yohanpaya74@hotmail.fr>
2024-12-09 08:52:39 +0100

    Add account serializer test

--- back/back_account_managment/tests/test_serializer.py
+++ back/back_account_managment/tests/test_serializer.py
@@ -3,11 +3,15 @@ from back_account_managment.models import (
     AccountUser,
     AccountUserPermission,
 )
-from back_account_managment.serializers import AccountUserPermissionsSerializer
+from back_account_managment.serializers import (
+    AccountSerializer,
+    AccountUserPermissionsSerializer,
+)
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
 User = get_user_model()
 
@@ -24,9 +28,84 @@ class UserAccountUserSerializerTest(TestCase):
     """Nothing to test"""
 
 
+# TODO: refactor
 class AccountSerializerTest(TestCase):
-    def setUp(cls):
-        pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(
+            username="test", email="test@test.test"
+        )
+
+        self.account = Account.objects.create(name="test", user=self.user)
+
+        self.account_user = AccountUser.objects.create(
+            account=self.account, user=self.user
+        )
+
+        self.permission = Permission.objects.get(codename="view_account")
+
+        self.account_user_permission = AccountUserPermission.objects.create(
+            account_user=self.account_user, permissions=self.permission
+        )
+
+    def test_get_permissions_for_account_owner(self):
+        request = self.factory.get("/")
+        request.user = self.user
+
+        serializer = AccountSerializer(context={"request": request})
+
+        permissions = serializer.get_permissions(self.account)
+
+        self.assertEqual(
+            permissions,
+            [
+                "view_account",
+                "add_account",
+                "change_account",
+                "delete_account",
+            ],
+        )
+
+    def test_get_permissions(self):
+        user2 = User.objects.create(username="user2", email="user@user2.test")
+        request = self.factory.get("/")
+        request.user = user2
+
+        account_user = AccountUser.objects.create(
+            account=self.account, user=user2
+        )
+        AccountUserPermission.objects.create(
+            account_user=account_user, permissions=self.permission
+        )
+
+        serializer = AccountSerializer(context={"request": request})
+
+        permissions = serializer.get_permissions(self.account)
+
+        self.assertEqual(
+            permissions,
+            [
+                "view_account",
+            ],
+        )
+
+    def test_no_context(self):
+        serializer = AccountSerializer()
+
+        with self.assertRaises(KeyError):
+            serializer.get_permissions(self.account)
+
+    def test_user_is_not_a_contributor(self):
+        new_user = User.objects.create(
+            username="newUser", email="new@user.test"
+        )
+
+        request = self.factory.get("/")
+        request.user = new_user
+
+        with self.assertRaises(AccountUser.DoesNotExist):
+            serializer = AccountSerializer(context={"request": request})
+            serializer.get_permissions(self.account)
 
 
 class ItemSerializerTest(TestCase):
