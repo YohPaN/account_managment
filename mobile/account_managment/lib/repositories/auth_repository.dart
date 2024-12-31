2a48c98289a4d3680264609a46ad500258c44c0b
YohPaN <yohanpaya74@hotmail.fr>
2024-12-31 11:04:43 +0100

    Connexion auto Fixes #162

--- mobile/account_managment/lib/repositories/auth_repository.dart
+++ mobile/account_managment/lib/repositories/auth_repository.dart
@@ -3,10 +3,12 @@ import 'package:account_managment/models/repo_reponse.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthRepository {
+  final modelUrl = "token";
+
   Future<RepoResponse> login(String username, String password) async {
     final RepoResponse responseData = await RequestHandler.handleRequest(
         method: "POST",
-        uri: "token/",
+        uri: "$modelUrl/",
         contentType: "application/json",
         needAuth: false,
         body: {
@@ -20,12 +22,25 @@ class AuthRepository {
   Future<RepoResponse> refreshToken() async {
     final RepoResponse repoResponse = await RequestHandler.handleRequest(
         method: "POST",
-        uri: "token/refresh/",
+        uri: "$modelUrl/refresh/",
         contentType: "application/json",
         needAuth: false,
         body: {
           'refresh':
               await const FlutterSecureStorage().read(key: "refreshToken"),
+        });
+
+    return repoResponse;
+  }
+
+  Future<RepoResponse> verifyToken({required String token}) async {
+    final RepoResponse repoResponse = await RequestHandler.handleRequest(
+        method: "POST",
+        uri: "$modelUrl/verify/",
+        contentType: "application/json",
+        needAuth: false,
+        body: {
+          'token': token,
         });
 
     return repoResponse;
